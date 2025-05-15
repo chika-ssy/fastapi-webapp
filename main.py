5c5252bf6eac9624dd5a9fcc875d0cc445cfca69
[chika-ssy] <[sen.chika12321@gmail.com]>
2025-05-16 01:52:33 +0900

    im very tired

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, Request, Form, Query,HTTPException
-from fastapi import FastAPI, Request, Form, Query
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
